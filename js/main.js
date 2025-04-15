06399eb8a7a0086c50ae1b61ec8a462407362809
bebopmot <bebopmotinc@gmail.com>
2025-04-15 16:37:25 +0200

    added gameover logic

--- js/main.js
+++ js/main.js
@@ -1,155 +1,142 @@
 class Player {
-    constructor(){
-        this.positionX = 1;
-        this.positionY = 50;
-        this.width = 2
-        this.height = 10
-        
-    }
-    moveUp(){
-        this.positionY++;
-        const playerElm = document.getElementById("player");
-        playerElm.style.bottom = `${this.positionY}vh`;
+  constructor() {
+    this.positionX = 1;
+    this.positionY = 50;
+    this.width = 2;
+    this.height = 10;
+  }
+  moveUp() {
+    this.positionY++;
+    const playerElm = document.getElementById("player");
+    playerElm.style.bottom = `${this.positionY}vh`;
 
-        console.log ("moving up", this.positionY)
-    }
+    console.log("moving up", this.positionY);
+  }
 
-    moveDown(){
-        this.positionY--;
-        const playerElm = document.getElementById("player");
-        playerElm.style.bottom = `${this.positionY}vh`;
-        console.log ("moving down", this.positionY)
-    }
+  moveDown() {
+    this.positionY--;
+    const playerElm = document.getElementById("player");
+    playerElm.style.bottom = `${this.positionY}vh`;
+    console.log("moving down", this.positionY);
+  }
 }
-
-
 
 class Opponent {
-    constructor(){
-        this.positionX = 94;
-        this.positionY = 50;
-        this.width = 2
-        this.height = 40
-        this.speed = 2; 
-        this.direction = 'up'; 
-        
-    }
-    moveUp(){
-        this.positionY += this.speed;
-        const oppoElm = document.getElementById("opponent");
-        oppoElm.style.bottom = `${this.positionY}vh`;
-    }
+  constructor() {
+    this.positionX = 94;
+    this.positionY = 50;
+    this.width = 2;
+    this.height = 40;
+    this.speed = 2;
+    this.direction = "up";
+  }
+  moveUp() {
+    this.positionY += this.speed;
+    const oppoElm = document.getElementById("opponent");
+    oppoElm.style.bottom = `${this.positionY}vh`;
+  }
 
-    moveDown(){
-        this.positionY -= this.speed;
-        const oppoElm = document.getElementById("opponent");
-        oppoElm.style.bottom = `${this.positionY}vh`;
-    }
-    autoMove(){
-        setInterval(() => {
-            if (this.direction === 'up') {
-                this.moveUp();
-                if (this.positionY >= 100 - this.height) {
-                    this.direction = 'down';
-                }
-            } else {
-                this.moveDown();
-                if (this.positionY <= 0) {
-                    this.direction = 'up';
-                }
-            }
-        }, 100); // Adjust timing for desired speed
-    }
-
+  moveDown() {
+    this.positionY -= this.speed;
+    const oppoElm = document.getElementById("opponent");
+    oppoElm.style.bottom = `${this.positionY}vh`;
+  }
+  autoMove() {
+    setInterval(() => {
+      if (this.direction === "up") {
+        this.moveUp();
+        if (this.positionY >= 100 - this.height) {
+          this.direction = "down";
+        }
+      } else {
+        this.moveDown();
+        if (this.positionY <= 0) {
+          this.direction = "up";
+        }
+      }
+    }, 100); // Adjust timing for desired speed
+  }
 }
-
-
-
-
-
 
 class Ball {
-    constructor(){
-        this.positionX = 50
-        this.positionY = 50
-        this.width = 1
-        this.height = 2
-        this.speedX = Math.random() > 0.5 ? 1 : -1;
-        this.speedY = Math.random() > 0.5 ? 1 : -1;
+  constructor() {
+    this.positionX = 50;
+    this.positionY = 50;
+    this.width = 1;
+    this.height = 2;
+    this.speedX = Math.random() > 0.5 ? 1 : -1;
+    this.speedY = Math.random() > 0.5 ? 1 : -1;
+  }
+  move() {
+    // calculate the new position
+    this.positionX = this.positionX + this.speedX;
+    this.positionY += this.speedY;
+
+    // update the ui to reflect the new position
+    const ballElm = document.getElementById("ball");
+    ballElm.style.left = `${this.positionX}vw`;
+    ballElm.style.bottom = `${this.positionY}vh`;
+
+    if (
+      this.positionX <= player.positionX + player.width &&
+      this.positionX + this.width >= player.positionX && // Check if ball's right side reaches player's left side
+      this.positionY < player.positionY + player.height &&
+      this.positionY + this.height > player.positionY
+    ) {
+      // Ensure entire vertical overlap with paddle
+
+      this.positionX = player.positionX + player.width; // Adjust ball position outside paddle
+      this.speedX *= -1;
     }
-    move(){
-        // calculate the new position
-        this.positionX = this.positionX + this.speedX;
-        this.positionY += this.speedY;
 
-        // update the ui to reflect the new position
-        const ballElm = document.getElementById("ball");
-        ballElm.style.left = `${this.positionX}vw`;
-        ballElm.style.bottom = `${this.positionY}vh`;
-
-
-        if (
-          this.positionX <= player.positionX + player.width &&
-          this.positionX + this.width >= player.positionX && // Check if ball's right side reaches player's left side
-          this.positionY < player.positionY + player.height &&
-          this.positionY + this.height > player.positionY
-        ) {
-          // Ensure entire vertical overlap with paddle
-
-          this.positionX = player.positionX + player.width; // Adjust ball position outside paddle
-          this.speedX *= -1;
-        }
-
-
-        if (
-            this.positionX + this.width >= opponent.positionX &&
-            this.positionX <= opponent.positionX + opponent.width &&
-            this.positionY < opponent.positionY + opponent.height &&
-            this.positionY + this.height > opponent.positionY
-        ) {
-            this.positionX = opponent.positionX - this.width;
-            this.speedX *= -1;
-        }
-
-
-
-
-
-
-
-
-        //Top and bottom border collision
-        if(this.positionY >= 100 - this.height) {  
-            this.positionY = 100 - this.height; 
-            this.speedY *= -1; 
-        }
-        if(this.positionY <= 1) { 
-            this.positionY = 1; 
-            this.speedY *= -1; 
-        }
-
+    if (
+      this.positionX + this.width >= opponent.positionX &&
+      this.positionX <= opponent.positionX + opponent.width &&
+      this.positionY < opponent.positionY + opponent.height &&
+      this.positionY + this.height > opponent.positionY
+    ) {
+      this.positionX = opponent.positionX - this.width;
+      this.speedX *= -1;
     }
+
+    //Top and bottom border collision
+    if (this.positionY >= 100 - this.height) {
+      this.positionY = 100 - this.height;
+      this.speedY *= -1;
+    }
+    if (this.positionY <= 1) {
+      this.positionY = 1;
+      this.speedY *= -1;
+    }
+
+    if (this.positionX < 0 || this.positionX > 100) {
+        console.log("game over...");
+        location.href = "gameover.html";
+    }
+  }
+
+  gameOver() {
+    clearInterval(gameInterval);
+    alert("Game Over!");
+    
+  }
 }
-
 
 const ball = new Ball();
 
-const timerId = setInterval(() => {
+const gameInterval = setInterval(() => {
     ball.move();
 }, 80);
 
+const player = new Player();
 
-
-const player = new Player()
-
-document.addEventListener("keydown", (e) =>{
-if(e.code === 'ArrowUp'){
+document.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowUp") {
     player.moveUp();
-}else if (e.code === 'ArrowDown'){
+  } else if (e.code === "ArrowDown") {
     player.moveDown();
-}
+  }
 });
 
 const opponent = new Opponent();
 opponent.autoMove();
-
